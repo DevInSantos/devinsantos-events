b3857a836aa20e8c82c996a026c3ba1300ebd2eb
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2012-10-27 11:59:37 -0200

    User: adding mass assignment specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "mass assignment" do
+    context "allowed attributes" do
+      [:email, :password, :password_confirmation, :remember_me].each do |attributes|
+        it { should allow_mass_assignment_of attributes }
+      end
+    end
+  end
 end
