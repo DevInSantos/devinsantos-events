a8857c92f815dcf1dbc1e6867af542383eaf2325
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-12-01 13:49:58 -0200

    Seedzinho para testar o gravatar_image_tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
 User.create :email => 'admin@devinsantos.com.br', :password => '123456', :password_confirmation => '123456'
+
+Speaker.find_or_create_by_name "Ricardo Valeriano",
+              email: "ricardo.valeriano@gmail.com"
