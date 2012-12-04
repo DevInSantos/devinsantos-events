d10eecfb1d80e58a7e04179540630736f5fc6cd3
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2012-12-03 23:07:56 -0200

    Adicionando sponsorship type como enum

---
+++ app/enumerations/sponsorship_type.rb
@@ -0,0 +1,3 @@
+class SponsorshipType < EnumerateIt::Base
+  associate_values :gold, :silver, :bronze
+end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -3,5 +3,11 @@ class Sponsor < ActiveRecord::Base
 
   belongs_to :event
 
+  has_enumeration_for :sponsorship_type, :create_helpers => true
+
   scope :sponsoring, lambda { |event_id| where(:event_id => event_id) }
+
+  SponsorshipType.list.each do |sponsorship|
+    scope sponsorship.to_sym, where(:sponsorship_type => sponsorship)
+  end
 end
