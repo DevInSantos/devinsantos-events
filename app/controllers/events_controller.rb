75b23cde38a68b7f29aca1d60586173c93d9baeb
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2012-12-10 22:29:30 -0200

    Select desnecessário caso acessar solicitando json

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,14 @@
 class EventsController < ApplicationController
   def index
-    @event = Event.current
-    @track = Track.new(IntervalDecorator.decorate(@event.intervals).to_ary, TalkDecorator.decorate(@event.talks).to_ary)
 
     respond_to do |format|
-      format.html { render :index }
       format.json { render :json => Event.limit(5) }
+      format.html do
+        @event = Event.current
+        @track = Track.new(IntervalDecorator.decorate(@event.intervals).to_ary, TalkDecorator.decorate(@event.talks).to_ary)
+
+        render :index
+      end
     end
   end
 end
