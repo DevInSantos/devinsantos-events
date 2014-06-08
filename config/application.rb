36d4301c23a54c766c729771fb5287434e3f7fc1
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2014-06-08 18:53:43 -0300

    Fix I18n error

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,12 @@ module Devinsantos
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :"pt-BR"
     config.encoding = "utf-8"
-    
+
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
+
     config.assets.paths << Rails.root.join("fonts")
   end
 end
