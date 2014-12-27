b41219435ceeed533f7a1cc21cb26168ae50ad1a
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2014-12-27 00:21:01 -0200

    Update Ruby 2.2.0 and Rails 4.2.0 ;)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-ruby "2.1.5"
+ruby "2.2.0"
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.10'
+gem 'rails', '4.2.0'
 gem 'pg'
 gem 'foreigner'
 gem 'validators'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,6 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
   config.expect_with :rspec do |config|
@@ -34,10 +27,4 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  #config.order = "random"
 end
