d9cb848eaeb94efdf11ac3973f1f5de9edd20c14
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2015-03-06 15:59:39 -0300

    add compress to memcache

--- config/initializers/rack_cache.rb
+++ config/initializers/rack_cache.rb
@@ -1,12 +1,13 @@
 Rails.application.configure do
   servers = ENV["MEMCACHIER_SERVERS"].try(:split, ',')
   client = Dalli::Client.new(servers,
-                             :username => ENV["MEMCACHIER_USERNAME"] || "",
-                             :password => ENV["MEMCACHIER_PASSWORD"] || "",
-                             :failover => true,
-                             :socket_timeout => 1.5,
-                             :socket_failure_delay => 0.2,
-                             :value_max_bytes => 10485760)
+                             username: ENV["MEMCACHIER_USERNAME"] || "",
+                             password: ENV["MEMCACHIER_PASSWORD"] || "",
+                             failover: true,
+                             compress: true,
+                             socket_timeout: 1.5,
+                             socket_failure_delay: 0.2,
+                             value_max_bytes: 10485760)
 
 
   config.action_dispatch.rack_cache = {
